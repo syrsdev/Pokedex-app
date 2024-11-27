a70b9853390bf6fd66354b7751649baef27127da
Surya <surya.nata.aardhana@gmail.com>
2024-11-27 13:35:15 +0700

    style: create discover screen

---
+++ lib/screens/discover_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xffECE6F0),
+      body: SingleChildScrollView(child: Column()),
+    );
+  }
+}
