5e39bee299ddb24264b247533917fb417266c73c
Surya <surya.nata.aardhana@gmail.com>
2024-11-27 23:39:51 +0700

    feat: data pokemon type

---
+++ lib/data/elements.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:my_pokedex_app/model/elements.dart';
+
+final List<TypePokemon> typePokemonList = [
+  TypePokemon(
+    name: 'Fire type',
+    typeColor: const Color(0xffB72626),
+  ),
+  TypePokemon(
+    name: 'Water type',
+    typeColor: const Color(0xff0089BB),
+  ),
+  TypePokemon(
+    name: 'Grass type',
+    typeColor: const Color(0xff6FC506),
+  ),
+  TypePokemon(
+    name: 'Electric type',
+    typeColor: const Color(0xffE0BC05),
+  ),
+];
---
+++ lib/model/elements.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TypePokemon {
+  String name;
+  Color typeColor;
+
+  TypePokemon({
+    required this.name,
+    required this.typeColor,
+  });
+}
