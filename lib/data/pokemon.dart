8b14f7eb44f21749407e1088a74d9ac501a4a4c7
Surya <surya.nata.aardhana@gmail.com>
2024-11-28 19:31:09 +0700

    chore: dummy data

--- lib/data/pokemon.dart
+++ lib/data/pokemon.dart
@@ -13,6 +13,78 @@ final List<Pokemon> pokemonList = [
     image: "assets/images/bulbasaur.png",
   ),
   Pokemon(
+    name: "venusaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/venusaur.png",
+  ),
+  Pokemon(
+    name: "Charmander",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Fire", typeColor: const Color(0xffB72626)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/charmender.png",
+  ),
+  Pokemon(
+    name: "bulbasaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/bulbasaur.png",
+  ),
+  Pokemon(
+    name: "bulbasaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/bulbasaur.png",
+  ),
+  Pokemon(
+    name: "bulbasaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/bulbasaur.png",
+  ),
+  Pokemon(
+    name: "bulbasaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/bulbasaur.png",
+  ),
+  Pokemon(
+    name: "bulbasaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/bulbasaur.png",
+  ),
+  Pokemon(
+    name: "bulbasaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 6.9,
+    height: 0.7,
+    image: "assets/images/bulbasaur.png",
+  ),
+  Pokemon(
     name: "bulbasaur",
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
