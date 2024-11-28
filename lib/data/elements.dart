724f28f5ac435dc683f495f342a7e4cce74d5d79
Surya <surya.nata.aardhana@gmail.com>
2024-11-28 18:52:16 +0700

    chore: fix name value

--- lib/data/elements.dart
+++ lib/data/elements.dart
@@ -3,19 +3,19 @@ import 'package:my_pokedex_app/model/elements.dart';
 
 final List<TypePokemon> typePokemonList = [
   TypePokemon(
-    name: 'Fire type',
+    name: 'Fire',
     typeColor: const Color(0xffB72626),
   ),
   TypePokemon(
-    name: 'Water type',
+    name: 'Water',
     typeColor: const Color(0xff0089BB),
   ),
   TypePokemon(
-    name: 'Grass type',
+    name: 'Grass',
     typeColor: const Color(0xff6FC506),
   ),
   TypePokemon(
-    name: 'Electric type',
+    name: 'Electric',
     typeColor: const Color(0xffE0BC05),
   ),
 ];
