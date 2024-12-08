faaee301e41af30d8aff5188a2720271302f59b9
Surya <surya.nata.aardhana@gmail.com>
2024-12-08 22:23:06 +0700

    style: detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,13 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:my_pokedex_app/model/pokemon.dart';
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+class DetailScreen extends StatefulWidget {
+  final Pokemon pokemon;
 
+  const DetailScreen({super.key, required this.pokemon});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Detail Screen"),
+        automaticallyImplyLeading: true,
+        backgroundColor: widget.pokemon.type.typeColor,
+        iconTheme: const IconThemeData(color: Colors.white),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(200),
+            child: SizedBox(
+              height: 10,
+              child: Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Positioned(
+                    bottom: -50,
+                    child: Image.asset(
+                      widget.pokemon.image,
+                      width: 250,
+                      height: 250,
+                    ),
+                  )
+                ],
+              ),
+            )),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+          child: Column(
+            children: [
+              Center(
+                  child: Text(
+                widget.pokemon.name,
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
