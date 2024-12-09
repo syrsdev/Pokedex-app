1e38fff74f0f43eaa57635a95fa380b87ee7c746
Surya <surya.nata.aardhana@gmail.com>
2024-12-09 15:23:27 +0700

    style: detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -11,6 +11,7 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  bool isFavorite = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,15 +49,108 @@ class _DetailScreenState extends State<DetailScreen> {
           margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
           child: Column(
             children: [
-              Center(
-                  child: Text(
+              Text(
                 widget.pokemon.name,
-                style:
-                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ))
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
+                child: Container(
+                  color: widget.pokemon.type.typeColor,
+                  width: 137,
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.pokemon.type.name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                widget.pokemon.description,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("Weight:"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                        child: Container(
+                          color: const Color(0xffECE6F0),
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: 50,
+                          child: Center(
+                            child: Text(widget.pokemon.weight.toString()),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("height:"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                        child: Container(
+                          color: const Color(0xffECE6F0),
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: 50,
+                          child: Center(
+                            child: Text(widget.pokemon.height.toString()),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isFavorite = !isFavorite;
+          });
+        },
+        child: isFavorite == true
+            ? const Icon(
+                Icons.favorite,
+                color: Colors.red,
+              )
+            : const Icon(Icons.favorite_border),
       ),
     );
   }
