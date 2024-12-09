27e87450ce39b727319878d300216e3191082a96
Surya <surya.nata.aardhana@gmail.com>
2024-12-09 15:23:15 +0700

    add: more data pokemon

--- lib/data/pokemon.dart
+++ lib/data/pokemon.dart
@@ -8,8 +8,8 @@ final List<Pokemon> pokemonList = [
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
-    weight: 6.9,
-    height: 0.7,
+    weight: 100.0,
+    height: 2.0,
     image: "assets/images/venusaur.png",
   ),
   Pokemon(
@@ -17,12 +17,12 @@ final List<Pokemon> pokemonList = [
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     type: TypePokemon(name: "Fire", typeColor: const Color(0xffB72626)),
-    weight: 6.9,
-    height: 0.7,
+    weight: 8.5,
+    height: 0.6,
     image: "assets/images/charmender.png",
   ),
   Pokemon(
-    name: "bulbasaur",
+    name: "Bulbasaur",
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
@@ -31,30 +31,66 @@ final List<Pokemon> pokemonList = [
     image: "assets/images/bulbasaur.png",
   ),
   Pokemon(
-    name: "wartortle",
+    name: "Wartortle",
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     type: TypePokemon(name: "Water", typeColor: const Color(0xff009DCD)),
-    weight: 6.9,
-    height: 0.7,
+    weight: 22.5,
+    height: 1.0,
     image: "assets/images/wartortle.png",
   ),
   Pokemon(
-    name: "blaistoise",
+    name: "Blaistoise",
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     type: TypePokemon(name: "Water", typeColor: const Color(0xff009DCD)),
-    weight: 6.9,
-    height: 0.7,
+    weight: 85.5,
+    height: 1.6,
     image: "assets/images/blaistois.png",
   ),
   Pokemon(
-    name: "charmeleon",
+    name: "Charmeleon",
     description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     type: TypePokemon(name: "Fire", typeColor: const Color(0xffB72626)),
-    weight: 6.9,
-    height: 0.7,
+    weight: 19.0,
+    height: 1.1,
     image: "assets/images/charmeleon.png",
+  ),
+  Pokemon(
+    name: "Charizard",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Fire", typeColor: const Color(0xffB72626)),
+    weight: 90.5,
+    height: 1.7,
+    image: "assets/images/charizard.png",
+  ),
+  Pokemon(
+    name: "Squirtle",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Water", typeColor: const Color(0xff009DCD)),
+    weight: 9.0,
+    height: 0.5,
+    image: "assets/images/squirtle.png",
+  ),
+  Pokemon(
+    name: "Pikachu",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Electric", typeColor: const Color(0xffE0BC05)),
+    weight: 6.0,
+    height: 0.4,
+    image: "assets/images/pikachu.png",
+  ),
+  Pokemon(
+    name: "Ivysaur",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    type: TypePokemon(name: "Grass", typeColor: const Color(0xff6FC506)),
+    weight: 13.0,
+    height: 1.0,
+    image: "assets/images/ivysaur.png",
   ),
 ];
